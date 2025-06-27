4475e3de9f145bcf6a8c9835cf4994944a7362b5
hernanbarrera762 <hernanbarrera762@gmail.com>
2025-06-27 18:33:56 -0300

    correción ejercicio-6

--- ejercicio-5/src/App.js
+++ ejercicio-5/src/App.js
@@ -1,17 +1,17 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 
 function CalculadoraReact() {
   const [num1, setNum1] = useState('');
-  const [num2,setNum2] = useState('');
-  const [operacion,setOperacion] = useState('suma');
+  const [num2, setNum2] = useState('');
+  const [operacion, setOperacion] = useState('suma');
   const [resultado, setResultado] = useState(null);
 
   const manejarCalculo = () => {
     const numero1 = parseFloat(num1);
-    const numero2= parseFloat(num2);
+    const numero2 = parseFloat(num2);
 
     if (isNaN(numero1) || isNaN(numero2)) {
-      setResultado('Por favor, ingrese números válidos.');
+      setResultado('Por favor, ingrese números validos.');
       return;
     }
 
@@ -26,12 +26,20 @@ function CalculadoraReact() {
       case 'multiplicacion':
         res = numero1 * numero2;
         break;
-
+      case 'division':
+        if (numero2 === 0) {
+          res = 'Error: No se puede dividir por cero.';
+        } else {
+          res = numero1 / numero2;
+        }
+        break;
       default:
         return;
     }
     setResultado(res);
   };
+
+  const esBotonDeshabilitado = operacion === 'division' && (num1 === '0' || num2 === '0');
 
   return (
     <div>
@@ -60,7 +68,7 @@ function CalculadoraReact() {
 
       <button
         onClick={manejarCalculo}
-        disabled={operacion === 'division'}
+        disabled={esBotonDeshabilitado}
       >
         Calcular
       </button>
